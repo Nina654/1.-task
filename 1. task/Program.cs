d7f23af74440382442cade5907a6e4930a3db015
ninal <ninal@DESKTOP-NIT4FOL>
2024-08-05 21:56:55 +0200

    Add project files.

---
+++ 1. task/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Name:");
+String firstName = Console.ReadLine();
+Console.WriteLine("Last Name:");
+String lastName = Console.ReadLine();
+Console.WriteLine("Age:");
+int age = Convert.ToInt32(Console.ReadLine());
+if (age < 21)
+{
+    Console.WriteLine("You're not allowed to use the app");
+}
+
+else
+{
+    Console.WriteLine("Wellcome");
+}
